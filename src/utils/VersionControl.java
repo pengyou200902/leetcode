313fa90776c36e165cee39b9fd312d7eef0ea995
彭友 <pengyou200902@163.com>
2020-06-12 21:48:19 +0800

    add VersionControl.java
    Signed-off-by: 彭友 <pengyou200902@163.com>

---
+++ src/utils/VersionControl.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * @Author Friende.Peng_You
+ * @Date 2020-06-12 14:38
+ */
+public class VersionControl {
+    public boolean isBadVersion(int version) {
+        return false;
+    }
+}
