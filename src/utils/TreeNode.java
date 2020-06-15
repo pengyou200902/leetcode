155d40728defd007b582efcf401ad60236c2db22
彭友 <pengyou200902@163.com>
2020-06-15 21:55:59 +0800

    add TreeNode.java
    Signed-off-by: 彭友 <pengyou200902@163.com>

---
+++ src/utils/TreeNode.java
@@ -0,0 +1,18 @@
+package utils;
+
+/**
+ * @Author Friende.Peng_You
+ * @Date 2020-06-15 21:10
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
